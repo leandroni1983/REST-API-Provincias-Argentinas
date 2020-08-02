d98a2ab9cf1ef08f8873e132283847bfa8a6d1a9
leandroni1983 <leandromanzana@gmail.com>
2020-08-01 21:31:19 -0300

    Muestra REST API

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,Response
+from bson import json_util
+from baseapimongo import Baseprovincias
+
+
+host =""
+base ="mydatabase"
+b=Baseprovincias(base,host)
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_Home():
+    datos = b.gethome()
+    return render_template('index.html',c=datos)
+
+@app.route('/provincia')
+def get_provincias():
+    datos= b.getprovincias()
+    return Response(json_util.dumps(datos,indent=4, sort_keys=True),mimetype='application/json')
+
+@app.route('/provincia/<provincia_id>',methods=['GET'])
+def get_provincia(provincia_id):
+    datos =b.getprovincia(provincia_id)
+    return Response(json_util.dumps(datos,indent=4, sort_keys=True),mimetype='application/json')
+    
+@app.route('/localidades')
+def get_localidades():
+    datos =b.getlocalidades()
+    return Response(json_util.dumps(datos,indent=4, sort_keys=True),mimetype='application/json')
+
+@app.route('/localidades/<provincia_id>',methods=['GET'])
+def get_localidadesprovincia(provincia_id):
+    datos =b.getlocalidadesprovincia(provincia_id)
+    return Response(json_util.dumps(datos,indent=4, sort_keys=True),mimetype='application/json')
+
+
+
+#app.run(host="localhost")
+
+if __name__ == "__name__": app.run()
+ 
---
+++ baseapimongo.py
@@ -0,0 +1,32 @@
+from flask import Flask,jsonify,Response
+import pymongo
+from bson import json_util
+import json
+
+class Baseprovincias():
+
+    def __init__(self,base,host):
+        self.base = base
+        self.host = host
+        self.conn =  pymongo.MongoClient(self.host)
+        self.db = self.conn[self.base]
+
+    def gethome(self):
+        self.data = self.db.provincias.find({'$query':{},"$orderby":{"id" : 1 }},{ "_id": 0,'id':1,'nombre':1})
+        return self.data
+
+    def getprovincias(self):
+        self.data = self.db.provincias.find({},{ "_id": 0})
+        return self.data
+        
+    def getprovincia(self,provincia_id):
+        self.data = self.db.provincias.find({'id':provincia_id},{ "_id": 0})
+        return self.data
+
+    def getlocalidades(self):
+        self.data = self.db.data_provs.find({},{ "_id": 0})
+        return self.data
+    
+    def getlocalidadesprovincia(self,provincia_id):
+        self.data = self.db.data_provs.find({'provincia_id':provincia_id},{ "_id": 0})
+        return  self.data
